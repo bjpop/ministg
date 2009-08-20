25b63b3d3303a9e7bd637a4ef5ccc81fc5b14f87
bjpop <bjpop@csse.unimelb.edu.au>
2009-08-20 12:00:02 +0000

    update version number

--- src/Ministg/Options.hs
+++ src/Ministg/Options.hs
@@ -39,7 +39,7 @@ programName = "ministg"
 
 -- This should really come from the cabal file somehow.
 versionNumber :: String
-versionNumber = "0.1"
+versionNumber = "0.2"
 
 versionInfo :: String
 versionInfo = unwords [programName, "version", versionNumber]
